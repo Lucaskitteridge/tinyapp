cbcaca1f0ef181ca00f6c350225ffa215632be24
Lucas Kitteridge <lucas@MacBook.local>
2020-12-08 18:55:29 -0500

    Added login and logout feature

--- express_server.js
+++ express_server.js
@@ -1,9 +1,11 @@
 const bodyParser = require("body-parser");
+const cookieParser = require('cookie-parser')
 const express = require("express");
 const app = express();
 const PORT = 8080; 
 
 app.use(bodyParser.urlencoded({extended: true}));
+app.use(cookieParser())
 
 app.set("view engine", "ejs");
 
@@ -13,13 +15,13 @@ const urlDatabase = {
 };
 
 app.get("/urls", (req, res) => {
-  const templateVars = { urls: urlDatabase };
+  const templateVars = { urls: urlDatabase, username: req.cookies["username"]};
   res.render("urls_index", templateVars);
-
 });
 
 app.get("/urls/new", (req, res) => {
-  res.render("urls_new");
+  const templateVars = { username: req.cookies["username"]}
+  res.render("urls_new", templateVars);
 });
 
 app.post("/urls", (req, res) => {
@@ -33,6 +35,16 @@ app.post("/urls/:shortURL/delete", (req, res) => {
   res.redirect(`/urls`)
 })
 
+app.post("/login", (req, res) => {
+  res.cookie("username", req.body.username)
+  res.redirect(`/urls`)
+})
+
+app.post("/logout", (req, res) => {
+  res.clearCookie("username")
+  res.redirect(`/urls`)
+})
+
 app.post("/urls/:shortURL", (req, res) => {
   urlDatabase[req.params.shortURL] = req.body.longURL
   res.redirect(`/urls`)
@@ -40,7 +52,7 @@ app.post("/urls/:shortURL", (req, res) => {
 
 app.get("/urls/:shortURL", (req, res) => {
   const shortURL = req.params.shortURL
-  const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[shortURL]};
+  const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[shortURL], username: req.cookies["username"],};
   res.render("urls_show", templateVars);
 });
 
