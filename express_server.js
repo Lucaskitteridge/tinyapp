18853369b7977cb6dc3d74f660db105deafb9345
Lucas Kitteridge <lucas@MacBook.local>
2020-12-08 15:00:06 -0500

    edit button and function added

--- express_server.js
+++ express_server.js
@@ -29,8 +29,12 @@ app.post("/urls", (req, res) => {
 });
 
 app.post("/urls/:shortURL/delete", (req, res) => {
-  console.log('welcome')
   delete urlDatabase[req.params.shortURL]
+  res.redirect(`/urls`)
+})
+
+app.post("/urls/:shortURL", (req, res) => {
+  urlDatabase[req.params.shortURL] = req.body.longURL
   res.redirect(`/urls`)
 })
 
