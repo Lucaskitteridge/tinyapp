0341b8dae183964f594b9bf82acf415a174cd968
Lucas Kitteridge <lucas@MacBook.local>
2020-12-10 16:05:56 -0500

    moved a function over to helpers

---
+++ helpers.js
@@ -0,0 +1,12 @@
+//helper function
+const getUserByEmail = (email, database) => {
+  const keys = Object.keys(database)
+    for (let key of keys) {
+      const user = database[key]
+      if (user.email === email) {
+        return key
+      }
+    }    
+  }
+
+  module.exports = { getUserByEmail }
