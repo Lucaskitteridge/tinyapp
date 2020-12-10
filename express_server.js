b74d0d15b5d721735fe83cb2b915df0ed5084ea2
Lucas Kitteridge <lucas@MacBook.local>
2020-12-10 08:54:26 -0500

    linted

--- express_server.js
+++ express_server.js
@@ -1,8 +1,8 @@
 const bodyParser = require("body-parser");
-const cookieParser = require('cookie-parser')
+const cookieParser = require('cookie-parser');
 const express = require("express");
 const app = express();
-const PORT = 8080; 
+const PORT = 8080;
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(cookieParser());
@@ -18,19 +18,21 @@ const urlDatabase = {
 
 //rendering of the index page with all our urls
 app.get("/urls", (req, res) => {
-  const user = req.cookies["user_id"]
-  if(user){
-    const userUrls = urlsForUser(user.newId)
+  const user = req.cookies["user_id"];
+  if (user) {
+    const userUrls = urlsForUser(user.newId);
     const templateVars = { urls: userUrls, user: req.cookies["user_id"]};
     res.render("urls_index", templateVars);
-  } else {res.redirect('/login')}
+  } else {
+    res.redirect('/login');
+  }
 });
 
 //rendering of the page to add new urls
 app.get("/urls/new", (req, res) => {
-  const user = req.cookies["user_id"]
-  if(!user){
-    res.redirect("/urls")
+  const user = req.cookies["user_id"];
+  if (!user) {
+    res.redirect("/urls");
   }
   const templateVars = { user: req.cookies["user_id"]};
   res.render("urls_new", templateVars);
@@ -52,14 +54,14 @@ app.get("/login", (req, res) => {
 app.post("/register", (req, res) => {
   const newId = generateRandomString();
   //checking if email or password is blanck
-  if(req.body["email"] === '' || req.body["password"] === '') {
+  if (req.body["email"] === '' || req.body["password"] === '') {
     throw new Error(404);
   }
   //checking if email already exists
   if (users) {
     for (let ids in users) {
       if (users[ids].email === req.body["email"]) {
-      throw new Error(404);
+        throw new Error(404);
       }
     }
   }
@@ -69,13 +71,13 @@ app.post("/register", (req, res) => {
   users[newId] = newIdObject;
   res.cookie("user_id", users[newId]);
   res.redirect(`/urls`);
-})
+});
 
 app.post("/urls", (req, res) => {
-  const newSmallUrl = generateRandomString(); 
-  const user = req.cookies["user_id"]
-  urlDatabase[newSmallUrl] = {longURL :req.body.longURL, userID :user.newId}
-  res.redirect(`/urls/${newSmallUrl}`);     
+  const newSmallUrl = generateRandomString();
+  const user = req.cookies["user_id"];
+  urlDatabase[newSmallUrl] = {longURL :req.body.longURL, userID :user.newId};
+  res.redirect(`/urls/${newSmallUrl}`);
 });
 
 //Deleteing a url
@@ -93,10 +95,10 @@ app.post("/login", (req, res) => {
         res.redirect("/urls");
       } else {
         throw new Error(403);
-      } 
+      }
     }
   } else {
-    throw new Error(403)
+    throw new Error(403);
   }
 });
 
@@ -109,14 +111,21 @@ app.post("/logout", (req, res) => {
 app.post("/urls/:shortURL", (req, res) => {
   urlDatabase[req.params.shortURL].longURL = req.body.longURL;
   res.redirect(`/urls`);
-})
-
-app.get("/urls/:shortURL", (req, res) => {
-  const shortURL = req.params.shortURL;
-  const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[shortURL].longURL, user: req.cookies["user_id"],};
-  res.render("urls_show", templateVars);
 });
 
+//rendering of the edit page with the new short url
+app.get("/urls/:shortURL", (req, res) => {
+  const user = req.cookies["user_id"];
+  if (user) {
+    const shortURL = req.params.shortURL;
+    const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[shortURL].longURL, user: req.cookies["user_id"],};
+    res.render("urls_show", templateVars);
+  } else {
+    res.redirect('/login');
+  }
+});
+
+//when clicking on the key it takes you to the original website
 app.get("/u/:shortURL", (req, res) => {
   const longURL = urlDatabase[req.params.shortURL].longURL;
   res.redirect(longURL);
@@ -132,17 +141,17 @@ app.listen(PORT, () => {
 });
 
 //function to make a random 6 character code
-function generateRandomString() {
+const generateRandomString = () => {
   let r = Math.random().toString(36).substring(7);
   return ("random", r);
 };
 
 const urlsForUser = (id) => {
-  let newobject = {}
-  for(let shortURL in urlDatabase) {
-    if (id === urlDatabase[shortURL].userID){
-      newobject[shortURL] = urlDatabase[shortURL]
+  let newobject = {};
+  for (let shortURL in urlDatabase) {
+    if (id === urlDatabase[shortURL].userID) {
+      newobject[shortURL] = urlDatabase[shortURL];
     }
   }
-  return newobject
-}
+  return newobject;
+};
