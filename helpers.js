31e84255305289757f83dcb13d6f9e8e6c680239
Lucas Kitteridge <lucas@MacBook.local>
2020-12-10 22:42:06 -0500

    changed to const

--- helpers.js
+++ helpers.js
@@ -1,7 +1,7 @@
 //helper function
 const getUserByEmail = (email, database) => {
   const keys = Object.keys(database)
-    for (let key of keys) {
+    for (const key of keys) {
       const user = database[key]
       if (user.email === email) {
         return key
